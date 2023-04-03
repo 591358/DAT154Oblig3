df686a2a1853ab577347eeff04b922eee57e25e3
591358 <591358@stud.hvl.no>
2023-04-03 14:17:38 +0200

    inital commit

---
+++ LinqTest/Program.cs
@@ -0,0 +1,102 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using static Oblig3.Class1;
+
+namespace Oblig3
+{
+    internal class Class1
+    {
+
+        public static void Main(string[] args)
+        {
+            List<Course> courses = new()
+            {
+                new Course{coursename = "DAT110", coursecode = "110"},
+                new Course{coursename = "DAT154", coursecode = "154" }
+
+            };
+
+            List<Student> students = new()
+            {
+                new Student { name = "per", age = 12 , grade ="F", coursecode="110"},
+                new Student { name = "atle", age = 12 , grade ="F", coursecode="110"},
+                new Student { name = "johannes", age = 12 , grade ="F", coursecode="154"},
+                new Student{name = "knut", age =23, grade ="B", coursecode="154"},
+                new Student{name= "micro", age=30, grade="C", coursecode="110"},
+                new Student{name="perkeles", age=69,grade="B",  coursecode="110"}
+            };
+
+
+            List<Grade> grades = new()
+            {
+                  new Grade{grades = "F", coursename = "DAT110"},
+                  new Grade{grades = "B", coursename = "DAT154"},
+                  new Grade{grades = "C", coursename = "DAT110"},
+                  new Grade{grades = "A", coursename = "DAT109"},
+                  new Grade{grades = "D", coursename = "DAT107"},
+                  new Grade{grades = "F", coursename = "DAT108"}
+
+            };
+
+            string dat110 = "DAT110";
+            /* Console.WriteLine("Search for name: ");
+             string substring = Console.ReadLine();*/
+
+            /* var listOfStudentsMatchingSubstring = students.Where(a => a.name.Contains(substring));*/
+            var listOfStudentsAndGradesMatchingCourse = courses.Where(x => x.coursename.Equals("DAT154")).Join(students, sc => sc.coursecode, cc => cc.coursecode, (cc, sc) => new { cc.coursename, sc.name, sc.grade });
+
+
+
+            //var listOfgradesEqualOrBetter = grades.Where(grade => grade.grades.CompareTo("D") < 0);
+
+            /* foreach (var grde in listOfgradesEqualOrBetter)
+             {
+                 Console.WriteLine($"{grde.grades}");
+             }*/
+
+            /*foreach (var s in listOfStudentsMatchingSubstring)
+            {
+                Console.WriteLine(s.name);
+            }*/
+
+            /*  foreach (var t in listOfStudentsAndGradesMatchingCourse)
+              {
+                  Console.WriteLine($"Coursecode: {t.coursename} - name: {t.name} - grade: {t.grade}");
+              }*/
+
+
+
+        }
+        public class Student
+        {
+            public string name { get; set; }
+            public int age { get; set; }
+
+            public string grade { get; set; }
+
+            public string coursecode { get; set; }
+
+        }
+        public class Course
+        {
+            public string coursename { get; set; }
+            public string coursecode { get; set; }
+
+        }
+
+        public class Grade
+        {
+            public string grades { get; set; }
+
+            public string coursename { get; set; }
+
+
+
+        }
+    }
+}
---
+++ Oblig3/MainWindow.xaml.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using Oblig3.Models;
+using Microsoft.EntityFrameworkCore;
+using static System.Net.Mime.MediaTypeNames;
+using System.Windows.Controls.Primitives;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Oblig3
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Dat154Context dx = new();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Bindcombo();
+            DisplayFailures.MouseDown += new MouseButtonEventHandler(ButtonClick);
+
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+
+
+            DbSet<Student> students = dx.Students;
+            students.Load();
+
+
+            TestListView.DataContext =
+                students.OrderBy(s => s.Studentname).ToList();
+        }
+
+        private void OnTextBoxChanged(object sender, TextChangedEventArgs e)
+        {
+
+            String substring = StudNameTextBox.Text;
+            DbSet<Student> students = dx.Students;
+            students.Load();
+
+
+            TestListView.DataContext =
+                students.Where(a => a.Studentname.Contains(substring)).ToList();
+        }
+
+
+
+
+        private void Bindcombo()
+
+        {
+            DbSet<Course> courselists = dx.Courses;
+            DbSet<Grade> gradelist = dx.Grades;
+            DbSet<Student> students = dx.Students;
+            students.Load();
+            courselists.Load();
+            gradelist.Load();
+            var uniqueGrades = gradelist.GroupBy(a => a.Grade1).Select(y => y.First()).Distinct().ToList();
+            var x = courselists.Distinct().ToList();
+
+            CourseAlternatives.DataContext = x;
+            SelectName.DataContext = students.ToList();
+            SelectGrades.DataContext = uniqueGrades;
+            SelectCourse.DataContext = x;
+
+
+        }
+
+        private void CourseAlternatives_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            String coursenameString = CourseAlternatives.SelectedValue.ToString();
+            Console.WriteLine(coursenameString);
+            DbSet<Course> courses = dx.Courses;
+            DbSet<Student> students = dx.Students;
+            DbSet<Grade> grades = dx.Grades;
+            students.Load();
+            courses.Load();
+            grades.Load();
+            var studentAndGrades = courses.Where(x => x.Coursename.Equals(coursenameString)).Join(grades, cc => cc.Coursecode, gc => gc.Coursecode, (cc, gc) => new { cc.Coursename, gc.Student.Studentname, gc.Grade1 }).ToList();
+            TestListView.DataContext = studentAndGrades;
+
+        }
+
+        private void SelectGrades_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            String selectedGrade = SelectGrades.SelectedValue.ToString();
+            Console.WriteLine(selectedGrade);
+            DbSet<Course> courses = dx.Courses;
+            DbSet<Student> students = dx.Students;
+            DbSet<Grade> grades = dx.Grades;
+            students.Load();
+            courses.Load();
+            grades.Load();
+            var studentAndGrades = grades.Where(x => x.Grade1.CompareTo(selectedGrade) < 0).Join(courses, cc => cc.Coursecode, gc => gc.Coursecode, (sc, cc) => new { sc.Student.Studentname, cc.Coursename, sc.Grade1 }).ToList();
+            TestListView.DataContext = studentAndGrades;
+        }
+
+        private void DisplayFailures_Click(object sender, RoutedEventArgs e)
+        {
+            DbSet<Course> courses = dx.Courses;
+            DbSet<Student> students = dx.Students;
+            DbSet<Grade> grades = dx.Grades;
+            students.Load();
+            courses.Load();
+            grades.Load();
+            var studentAndGrades = grades.Where(x => x.Grade1.CompareTo("F") == 0).Join(courses, cc => cc.Coursecode, gc => gc.Coursecode, (sc, cc) => new { sc.Student.Studentname, cc.Coursename, sc.Grade1 }).ToList();
+            TestListView.DataContext = studentAndGrades;
+        }
+
+        private void SelectStudent(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void SelectName_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+
+            SelectCourse.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void SelectCourse_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            String selectedName = SelectName.SelectedValue.ToString();
+            String courseName = SelectCourse.SelectedValue.ToString();
+
+            DbSet<Course> courses = dx.Courses;
+            DbSet<Student> students = dx.Students;
+            DbSet<Grade> grades = dx.Grades;
+            students.Load();
+            courses.Load();
+            grades.Load();
+            var attendee = courses.Where(a => a.Coursename.Equals(courseName)).Join(grades, cr => cr.Coursecode, gr => gr.Coursecode, (cr, gr) => new { gr.Student.Studentname, cr.Coursename, gr.Grade1 }).Where(n => n.Studentname.Equals(selectedName));
+
+            if (attendee.IsNullOrEmpty())
+            {
+                Course course = courses.Where(a => a.Coursename.Equals(courseName)).First();
+                Student updatedStudent = students.Where(a => a.Studentname.Equals(selectedName)).First();
+                Grade grade = new Grade();
+
+                grade.Student = updatedStudent;
+                grade.Studentid = updatedStudent.Id;
+                grade.CoursecodeNavigation = course;
+                grade.Coursecode = course.Coursecode;
+                grade.Grade1 = "";
+
+                grades.Add(grade);
+                dx.Grades.Add(grade);
+                dx.SaveChanges();
+
+                attendee = courses.Where(a => a.Coursename.Equals(courseName)).Join(grades, cr => cr.Coursecode, gr => gr.Coursecode, (cr, gr) => new { gr.Student.Studentname, cr.Coursename, gr.Grade1 }).Where(n => n.Studentname.Equals(selectedName));
+                TestListView.DataContext = attendee.ToList();
+            }
+            else
+            {
+                TestListView.DataContext = attendee.ToList();
+            }
+
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+        public string Grade { get; set; }
+    }
+}
